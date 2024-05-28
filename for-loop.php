65440244aecd6a38f407f496ea7f154eab2cd424
donnyfauzi <donnyfauzi55@gmail.com>
2024-05-28 16:23:00 +0700

    tambah materi

---
+++ for-loop.php
@@ -0,0 +1,22 @@
+<?php
+
+$i = 10;
+
+// for ($i=1; $i <=15 ; $i++) { 
+//     echo $i . PHP_EOL;
+// }
+
+// for ($i=10; $i >= 1 ; $i--) { 
+//     echo $i . PHP_EOL;
+// }
+
+//atau bisa juga 
+
+for ($i=1; $i <=10 ; $i++) : 
+    echo $i . PHP_EOL;
+endfor;
+
+for ($i=10; $i >= 1 ; $i--) : 
+    echo $i . PHP_EOL;
+endfor;
+
---
+++ while-loop.php
@@ -0,0 +1,14 @@
+<?php
+
+$i = 1;
+
+// while ($i <= 10){ 
+//     echo $i . PHP_EOL ;
+//     $i++;
+// }
+
+//atau bisa juga 
+while ($i <= 10): 
+    echo $i . PHP_EOL ;
+    $i++;
+endwhile;
